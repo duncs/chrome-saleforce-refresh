b628ef40de1f91cbf337db6fc50d22a82d67bf14
Duncan Ferguson <duncan.ferguson@opsview.com>
2022-03-07 12:52:50 +0000

    Include the date in the console fresh log

--- sfdc_refresh.js
+++ sfdc_refresh.js
@@ -1,15 +1,13 @@
 window.onload = function () { 
-
     var incidents = document.querySelector('[title="Incidents"]');
 
     function my_refresh() {
         incidents.click();
-        console.log("REFRESH!");
+
+        console.log("REFRESH! - " + Date());
     }
 
 	my_refresh();
 
-	setInterval(function() {
-		my_refresh()}, 3600000)
-
- }
+	setInterval(function() { my_refresh() }, 3600000);
+}
